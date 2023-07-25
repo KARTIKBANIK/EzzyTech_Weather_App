aac640d4c86cde952b6b3cc77ca753023af0fff3
Kartik Banik <kartikbanikshishir@gmail.com>
2023-07-26 02:58:51 +0600

    adding files

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,139 @@
+import 'dart:convert';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:geolocator/geolocator.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:weather_forcust/widgets/temp_detail.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff47BFDF),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 40,
+                width: double.infinity,
+                child: Row(
+                  children: [
+                    Row(
+                      children: [
+                        FaIcon(
+                          Icons.location_on_outlined,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "Dhaka",
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        )
+                        // Text(
+                        //   "${mapOfWeather!["name"]}",
+                        //   style: TextStyle(
+                        //       fontSize: 25,
+                        //       color: Colors.white,
+                        //       fontWeight: FontWeight.bold),
+                        // ),
+                      ],
+                    ),
+                    Spacer(),
+                    Stack(
+                      children: [
+                        FaIcon(
+                          Icons.notifications,
+                          color: Colors.white,
+                        ),
+                        Positioned(
+                          right: 2,
+                          top: 4,
+                          child: CircleAvatar(
+                            radius: 4,
+                            backgroundColor: Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Image.asset("assets/images/cloud.png"),
+          ),
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: DetailsWeather(),
+
+              /*Container(
+                height: 300,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: Colors.white,
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      "${Jiffy(DateTime.now()).format('MMM do yy, h:mm a')}",
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      "${weatherMap!["main"]["temp"]} °C",
+                      style: TextStyle(
+                        fontSize: 60,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "${weatherMap!["weather"]["0"]["description"]} ",
+                      style: TextStyle(
+                        fontSize: 60,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      children: [Image.asset("assets/icons/wind.png")],
+                    ),
+                  ],
+                ),
+              ),*/
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
